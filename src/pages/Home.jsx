cc2c2c733ca0e117f321f8326cc6154489ed38a2
Rishab Singh <rishab.w.singh1234@gmail.com>
2023-07-28 19:33:36 +0530

    gsdgds

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,31 @@
 import React from 'react'
 import "../styles/home.css";
 
+import { BsCpu } from "react-icons/bs";
+import { BiMemoryCard } from "react-icons/bi";
+import { PiHardDriveLight } from "react-icons/pi";
+import { FaRunning } from "react-icons/fa";
 
 const Home = () => {
   return (
-      <div className='homePage fullXY withPadding'>
-        <div className="homePageStats">
-          <div className="dashboardCard">
-            
-          </div>
+    <div className='homePage fullXY withPadding'>
+      <div className="homePageStats">
+        <div className="dashboardCard roundSM">
+          <BsCpu size={40} />
+        </div>
+        <div className="dashboardCard roundSM">
+          <BiMemoryCard size={40} />
+        </div>
+        <div className="dashboardCard roundSM">
+
+          <PiHardDriveLight size={40} />
+        </div>
+        <div className="dashboardCard roundSM">
+
+          <FaRunning size={35} />
         </div>
       </div>
+    </div>
   )
 }
 
