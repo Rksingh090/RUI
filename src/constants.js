a2deb5090330cc2a96f71a47d0eba7bc04e1dc00
Rishab Singh <rishab.w.singh1234@gmail.com>
2023-08-09 01:38:36 +0530

    dfh

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-// export const WS_URL = "ws://"+window.location.hostname+":"+window.location.port;
-// export const API = "http://"+window.location.hostname+":"+window.location.port;
-export const WS_URL = "ws://localhost:9000";
-export const API = "http://localhost:9000";
+export const WS_URL = "ws://"+window.location.hostname+":"+window.location.port;
+export const API = "http://"+window.location.hostname+":"+window.location.port;
+// export const WS_URL = "ws://localhost:9000";
+// export const API = "http://localhost:9000";
