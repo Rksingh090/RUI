963a720741a680fabdbb57ce51e67ccff50c1313
Rishab Singh <rishab.w.singh1234@gmail.com>
2023-08-20 12:20:40 +0530

    sdg

--- src/constants.js
+++ src/constants.js
@@ -1,18 +1,18 @@
-// let protocol = new URL(window.location.href).protocol;
-// protocol = protocol.replace(":", "");
+let protocol = new URL(window.location.href).protocol;
+protocol = protocol.replace(":", "");
 
-// let WS_URL = "ws://"+window.location.hostname+":"+window.location.port;
-// let API = "http://"+window.location.hostname+":"+window.location.port;
+let WS_URL = "ws://"+window.location.hostname+":"+window.location.port;
+let API = "http://"+window.location.hostname+":"+window.location.port;
 
-// if(protocol === "https"){
-//     WS_URL = "wss://"+window.location.hostname+":"+window.location.port;
-//     API = "https://"+window.location.hostname+":"+window.location.port;
-// }
+if(protocol === "https"){
+    WS_URL = "wss://"+window.location.hostname+":"+window.location.port;
+    API = "https://"+window.location.hostname+":"+window.location.port;
+}
 
-// export {
-//     WS_URL,
-//     API
-// }
+export {
+    WS_URL,
+    API
+}
 
-export const WS_URL = "ws://localhost:9000";
-export const API = "http://localhost:9000";
+// export const WS_URL = "ws://localhost:9000";
+// export const API = "http://localhost:9000";
